ec084734f7f1c99d692149025acde5c1463a5cdc
yuanzhou.xu <yuanzhou.xu@aolc.cn>
2018-08-12 00:41:14 +0800

    init

--- App.js
+++ App.js
@@ -33,9 +33,9 @@ export default class App extends Component<Props> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    /*justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    backgroundColor: '#F5FCFF',*/
   },
   welcome: {
     fontSize: 20,
